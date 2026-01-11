dffb1841a7ab7d7ea0d4147727f43e6f00ffec7a
Mehwish-Mahmood <mehwishmahmood1205@gmail.com>
2026-01-11 11:59:55 +0530

    Add marks input for prediction model

--- app.py.py
+++ app.py.py
@@ -5,8 +5,9 @@ model = joblib.load("student_pass_model.pkl")
 st.title("Student Pass/Fail Prediction System")
 study_hours = st.number_input("Study Hours per Week", min_value=0.0, max_value=40.0, value=10.0)
 attendance = st.number_input("Attendance Percentage", min_value=0.0, max_value=100.0,value=75.0)
+marks = st.number_input("Marks", min_value=0.0, max_value=100.0,value=33.0)
 if st.button("Predict Result"):
-  input_data = np.array([[study_hours, attendance]])
+  input_data = np.array([[study_hours, attendance, marks]])
   prediction = model.predict(input_data)
   if prediction[0] == 1:
     st.success("Prediction: PASS")
